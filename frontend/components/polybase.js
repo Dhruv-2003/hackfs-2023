377e77e80abcb124fb667883ffdf9419ea6817e8
Architsharma7 <architsharma7911@gmail.com>
2023-06-18 14:26:29 +0530

    feat: polybase collections

--- frontend/components/polybase.js
+++ frontend/components/polybase.js
@@ -1,29 +1,76 @@
 import { Polybase } from "@polybase/client";
+import * as eth from "@polybase/eth"
 
 const db = new Polybase({
-  defaultNamespace: "your-namespace",
+  defaultNamespace:
+    "pk/0xdd6503afa34792ca49abce644c46527bc2f664299797958e7780d21b4713a9698d35124fa269561f078f89c4aea969a862a20021f3f4042e1d5e5803817e28d3/hackfs",
 });
 
-const createResponse = await db.applySchema(`
-  @public
-  collection CollectionName {
-    id: string;
-    name: string;
-    country : {
-        city: string,
-        pincode: number
-    };
-    age?: number ,
-    currency: map<string, number>
 
-    constructor (id: string, country: string) {
-      this.id = id;
-      this.country = country;
+const PolybaseUi = () => {
 
-      // Assign the public key of the user making the request to this record
-      if (ctx.publicKey)
-        this.publicKey = ctx.publicKey;
-    }
-    @index(name, age);
-  }
-`, 'your-namespace')
+  const createDB = async () => {
+
+    db.signer(async (data) => {
+      // A permission dialog will be presented to the user
+      const accounts = await eth.requestAccounts();
+    
+      // If there is more than one account, you may wish to ask the user which
+      // account they would like to use
+      const account = accounts[0];
+    
+      const sig = await eth.sign(data, account);
+    
+      return { h: "eth-personal-sign", sig };
+    })
+    
+    // for constructor   creating collection record
+    const collection = await db.collection("City").create(["new-york", "New York"]);
+    // for functions    updating
+    await db.collection("City").call("setCountry", ["USA"]);
+    //for reading
+    await db.collection("City").record("new-york").get();
+  
+  };
+
+  return (
+    <div>
+      <button onClick={createDB}>create db</button>
+    </div>
+  )
+}
+
+export default PolybaseUi
+
+// const createResponse = await db.applySchema(
+//   `
+// @public
+  // collection City {
+  //   id: string;
+  //   name: string;
+  //   country?: string;
+
+  //   constructor (id: string, name: string) {
+  //     this.id = id;
+  //     this.name = name;
+  //   }
+
+  //   setCountry (country: string) {
+  //     this.country = country;
+  //   }
+  // }
+
+  // @public
+  // collection Country {
+  //   id: string;
+  //   name: string;
+
+  //   constructor (id: string, name: string) {
+  //     this.id = id;
+  //     this.name = name;
+  //   }
+  // }
+// `,
+//   "your-namespace"
+// );
+
