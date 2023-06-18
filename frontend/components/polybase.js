5f8e86ef65583d625ae147dc88ca060e61f34287
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-18 14:28:13 +0530

    fix: polybase changes

--- frontend/components/polybase.js
+++ frontend/components/polybase.js
@@ -1,10 +1,12 @@
 import { Polybase } from "@polybase/client";
 
 const db = new Polybase({
-  defaultNamespace: "your-namespace",
+  defaultNamespace:
+    "pk/0xf868433a12a9d57e355176a00ee6b5c80ed1fe2c939d81062e0251081994f039d1ce57a55e66571da21ef0466200304dfb18ad7200533c44e83a036b5c088a42/ARK",
 });
 
-const createResponse = await db.applySchema(`
+const createResponse = await db.applySchema(
+  `
   @public
   collection CollectionName {
     id: string;
@@ -26,4 +28,10 @@ const createResponse = await db.applySchema(`
     }
     @index(name, age);
   }
-`, 'your-namespace')
+`,
+  "your-namespace"
+);
+
+const createCollectionRecord = await db
+  .collection("City")
+  .create(["new-york", "New York"]);
