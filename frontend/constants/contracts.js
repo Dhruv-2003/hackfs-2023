8ce78e08cbc3d7de526d4cf201e0a596077d279d
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-06-18 19:47:32 +0530

    chore: constants and context added

---
+++ frontend/constants/contracts.js
@@ -0,0 +1,10 @@
+export const ArkID_Address = "0xd4D3519150EC7bD1dBDf28d6D17160B46bf38eCC";
+
+export const MarketPlace_Address = "0x87ade1b53b5857cE44c848a2859dDf83116c8597";
+
+export const ERC6551Registery_Address =
+  "0xA673B8990566B9f5628cb44af0892F10515D6FF5";
+
+export const ERC6551Impl_Address = "0x29a3592BCcdB65f3183b3e755590b7EdB7527C1c";
+
+export const ARK_Address = "0x5ad70f724777efED9d4b6AB08Fb2Fbb827521C40";
